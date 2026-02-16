ed322a8e501ba47237376b01bf616a436139e4f8
Tomáš Polák <FrenkyBojler@users.noreply.github.com>
2026-02-16 19:07:53 +0100

    improve MyTextField focus handling

--- common/src/main/java/com/laposa/common/features/common/composables/MyTextField.kt
+++ common/src/main/java/com/laposa/common/features/common/composables/MyTextField.kt
@@ -1,6 +1,8 @@
 package com.laposa.common.features.common.composables
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.LocalTextStyle
@@ -9,14 +11,24 @@ import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.tv.material3.Border
+import androidx.tv.material3.Card
+import androidx.tv.material3.CardDefaults
+import androidx.tv.material3.MaterialTheme.colorScheme
 
 @Composable
 fun MyTextField(
@@ -52,6 +64,12 @@ fun MyTextField(
         safeFocusRequester?.focusRequester ?: FocusRequester()
     }
 
+    var isFocused by remember {
+        mutableStateOf(false)
+    }
+
+    val innerFocusRequester = remember { FocusRequester() }
+
     if (!visible) {
         safeFocusRequester?.reset()
         return
@@ -63,31 +81,76 @@ fun MyTextField(
         }
     }
 
-    TextField(
-        value = value,
-        onValueChange = onValueChange,
-        modifier = modifier
-            .focusRequester(focusRequester)
-            .testTag("text_field_${label?.toString()}"),
-        enabled = enabled,
-        readOnly = readOnly,
-        textStyle = textStyle,
-        label = label,
-        placeholder = placeholder,
-        leadingIcon = leadingIcon,
-        trailingIcon = trailingIcon,
-        prefix = prefix,
-        suffix = suffix,
-        supportingText = supportingText,
-        isError = isError,
-        visualTransformation = visualTransformation,
-        keyboardOptions = keyboardOptions,
-        keyboardActions = keyboardActions,
-        singleLine = singleLine,
-        maxLines = maxLines,
-        minLines = minLines,
-        interactionSource = interactionSource,
-        shape = shape,
-        colors = colors,
-    )
+    Card(
+        modifier =
+            Modifier
+                .height(70.dp)
+                .focusRequester(focusRequester)
+                .onFocusChanged {
+                    isFocused = it.isFocused
+                }
+                .testTag("text_field_${label?.toString()}"),
+        colors = CardDefaults.colors(
+            containerColor = Color.Transparent,
+        ),
+        onClick = { innerFocusRequester.requestFocus() },
+        border = CardDefaults.border(
+            border = if (!isFocused) {
+                Border(
+                    border = BorderStroke(
+                        1.dp,
+                        colorScheme.border
+                    )
+                )
+            } else Border(
+                border = BorderStroke(
+                    2.dp,
+                    Color.White,
+                )
+            ),
+            focusedBorder = Border(
+                border = BorderStroke(
+                    2.dp,
+                    Color.White,
+                )
+            )
+        ),
+        scale = CardDefaults.scale(focusedScale = 1.025f),
+    ) {
+        TextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = modifier
+                .focusRequester(innerFocusRequester)
+                .onFocusChanged {
+                    if(it.isFocused) {
+                        isFocused = true
+                    }
+                },
+            enabled = enabled,
+            readOnly = readOnly,
+            textStyle = textStyle,
+            label = label,
+            placeholder = placeholder,
+            leadingIcon = leadingIcon,
+            trailingIcon = trailingIcon,
+            prefix = prefix,
+            suffix = suffix,
+            supportingText = supportingText,
+            isError = isError,
+            visualTransformation = visualTransformation,
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            singleLine = singleLine,
+            maxLines = maxLines,
+            minLines = minLines,
+            interactionSource = interactionSource,
+            shape = shape,
+            colors = colors.copy(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            )
+        )
+    }
 }
